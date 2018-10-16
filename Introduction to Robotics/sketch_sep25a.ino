b4a4d44e8da9e1e5ed352ab0c06f6b9c8f205935
Neutrino <1784986256@qq.com>
2018-10-16 17:48:04 +0800

    Update Introduction to Robotics/sketch_sep25a.ino
    add zkm code of 18.10.16.17.48

--- Introduction to Robotics/sketch_sep25a.ino
+++ Introduction to Robotics/sketch_sep25a.ino
@@ -84,3 +84,88 @@ void loop() {
 		setSpeed(leftspeed=100, rightspeed=120);
 	}
 }
+
+/*
+TODO: merge code of zkm
+
+int in1 = 5;
+int in2 = 4;
+// motor two
+int in3 = 6;
+int in4 = 7;
+const int TrigPin =  9;
+const int EchoPin =  8;
+float cm;
+int Sensor_front_pin = 2;
+int Sensor_right_pin = 3;
+int value(){
+  digitalWrite(TrigPin, LOW);
+  //低高低电平发一丟�时间脉冲去TrigPin
+  delayMicroseconds(2);
+  digitalWrite(TrigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TrigPin, LOW);
+  float time_reply=pulseIn(EchoPin, HIGH); //the time from sent to receive
+  cm = time_reply /58.82;
+  Serial.println(cm);
+  if (cm >30 ) return 1;
+  else if (cm < 25) return 0;
+  else return 2;
+}
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+   Serial.begin(9600);
+   pinMode(TrigPin, OUTPUT);
+  pinMode(EchoPin, INPUT);
+}
+int detect(int Sensor_pin) {
+   int sensorValue = digitalRead(Sensor_pin);
+   Serial.print(Sensor_pin);
+   Serial.println(sensorValue);
+   delay(100);
+   return sensorValue;
+}
+int leftspeed = 80;
+int rightspeed = 95;
+void loop() {
+  // put your main code here, to run repeatedly:
+
+      digitalWrite(in2, LOW);
+      digitalWrite(in4, LOW);
+      int front = detect(Sensor_front_pin);
+      int right = detect(Sensor_right_pin);
+      int check = value();
+      if (front == 1){//no front
+        if (check == 1 ){ //too far
+          if (right == 0){
+            leftspeed = 80;
+            rightspeed = 100;
+            analogWrite(in3,rightspeed);
+            analogWrite(in1,leftspeed);
+            //delay(10);
+          }
+          //while(right == 1)
+          else{
+             leftspeed = 50;
+             rightspeed = 50;
+            analogWrite(in3,rightspeed);
+            analogWrite(in1,leftspeed);
+          }
+         }
+         else if(check == 2){//all right right
+             leftspeed = 80;
+             rightspeed = 95;
+            analogWrite(in1,leftspeed);
+            analogWrite(in3,rightspeed);
+         }
+           
+       else if(check == 0){//too close
+           leftspeed = 80;
+           rightspeed = 100;
+          an
+
+*/
